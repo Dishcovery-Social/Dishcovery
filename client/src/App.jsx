247f0303f38f1437a2598a3b4a4750991a53bc4f
Danielle Phillips <69817991+Peanut16@users.noreply.github.com>
2026-07-21 19:46:39 -0400

    refactor: to address comments
    added fonts
    change allignment of icons so they line up
    get rid of starting files
    added titles to icons for accessibility

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
