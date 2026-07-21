8a9885c1dbbd510c86109bdd42c8d7d6ef9a8047
Danielle Phillips <69817991+Peanut16@users.noreply.github.com>
2026-07-21 19:48:06 -0400

    fix: add missing component

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -6,13 +6,13 @@ export default function Sidebar() {
   return (
     <div className="mt-36 ml-12 flex flex-col gap-8">
       <Link to="/">
-        <div className="flex items-center gap-2 max-w-24 justify-end">
+        <div className="flex items-center gap-2 max-w-24">
           <img className="h-10" src={HomeIcon} alt="" />
           <h2>Home</h2>
         </div>
       </Link>
       <Link to="/account">
-        <div className="flex items-center gap-2 max-w-24 justify-end">
+        <div className="flex items-center gap-2 max-w-24">
           <img className="h-10" src={AccountIcon} alt="" />
           <h2 className="">Account</h2>
         </div>
