10bb5b1649f2748821d61e76cec48a32339a3888
Beatrice Olaosebikan <beat1rice2ola@gmail.com>
2026-07-25 19:43:19 -0500

    Set up database and tables (#58)

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,11 @@
+import cors from "cors";
 import express, { type Request, type Response } from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("Server is running.");
