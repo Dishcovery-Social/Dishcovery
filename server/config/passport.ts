d77d7ed49488cdda613857564063f5654e7f00a7
nathnet <nathachanok.n@gmail.com>
2026-07-25 23:19:25 -0700

    Fix to save avatar URL instead of profile URL

--- server/config/passport.ts
+++ server/config/passport.ts
@@ -9,13 +9,13 @@ const verify = async (
   profile: Profile,
   done: (error: unknown, user?: Express.User | false) => void,
 ) => {
-  const { id, username, profileUrl, emails } = profile;
+  const { id, username, photos, emails } = profile;
   try {
     const user = await UsersRepository.upsertUser({
       github_id: id,
       username: username ?? `user_${id}`,
       email: emails?.[0]?.value ?? null,
-      profile_image: profileUrl,
+      profile_image: photos?.[0]?.value ?? null,
     });
 
     return done(null, user);
