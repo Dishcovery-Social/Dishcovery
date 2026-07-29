1ba72725f110e26b8309e9a1ba8651fdf0129b53
Danielle Phillips <69817991+Peanut16@users.noreply.github.com>
2026-07-28 21:29:48 -0400

    feat: add recipe details page
    - add a route that will take the user to a more detailed version of the post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage.jsx";
+import RecipeDetails from "./pages/RecipeDetails.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/recipes/:id" element={<RecipeDetails />} />
     </Routes>
   );
 }
