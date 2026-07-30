03984465449a48539bffeb948002a680666847c5
Beatrice Olaosebikan <beat1rice2ola@gmail.com>
2026-07-29 22:23:36 -0500

    cherry picked to resolve commit issues

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -24,39 +24,33 @@ export default function HomePage() {
   }, []);
 
   return (
-    <div>
-      <Navbar />
-      <Sidebar />
-      <div className="flex flex-col items-center gap-12 mt-12">
-        {loading ? (
-          <p>Loading recipe...</p>
-        ) : error ? (
-          <p>Error: {error.message}</p>
-        ) : recipes.length === 0 ? (
-          <p>No recipes yet.</p>
-        ) : (
-          recipes.map((item) => (
-            <RecipeCard
-              key={item.id}
-              avatarUrl={item.profile_image}
-              username={item.username}
-              recipeId={item.id}
-              title={item.title}
-              instructions={item.instructions}
-              recipeImageUrl={item.image}
-            />
-          ))
-        )}
-      </div>
+    <>
+      {loading ? (
+        <p>Loading recipe...</p>
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : recipes.length === 0 ? (
+        <p>No recipes yet.</p>
+      ) : (
+        recipes.map((item) => (
+          <RecipeCard
+            key={item.id}
+            avatarUrl={item.profile_image}
+            username={item.username}
+            recipeId={item.id}
+            title={item.title}
+            instructions={item.instructions}
+            recipeImageUrl={item.image}
+          />
+        ))
+      )}
       <button
         type="button"
+        onClick={() => (window.location.href = "/create")}
         className="fixed right-8 bottom-8 h-20 z-10"
-        onClick={() => {
-          window.location.href = "/create";
-        }}
       >
         <img src={CreateIcon} alt="Create post" className="h-full" />
       </button>
-    </div>
+    </>
   );
 }
