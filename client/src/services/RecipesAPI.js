badf9201cdfbbb9ddbabd9fe7caad33439f9d4fa
nathnet <nathachanok.n@gmail.com>
2026-08-04 22:08:21 -0700

    Update to use SearchParams to auto handle encoding and decoding spaces

--- client/src/services/RecipesAPI.js
+++ client/src/services/RecipesAPI.js
@@ -1,7 +1,9 @@
 import { apiFetch } from "./apiUtils";
 
 export const getRecipes = async (category = null) => {
-  const query = category ? `?category=${category}` : "";
+  const params = new URLSearchParams();
+  if (category) params.set("category", category);
+  const query = params.toString() ? `?${params}` : "";
   return apiFetch(`/recipes${query}`);
 };
 
