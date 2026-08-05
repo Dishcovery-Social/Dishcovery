173c207419962eac78694551c1675cc654d542a4
Madyun Crazet <beat1rice2ola@gmail.com>
2026-08-05 02:03:57 -0500

    created recipe page

---
+++ client/src/pages/CreateRecipe.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import RecipeForm from "../components/RecipeForm.jsx";
+import { createRecipe } from "../services/RecipesAPI.js";
+
+const CreateRecipe = async () => {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [instructions, setInstructions] = useState("");
+  const [image, setImage] = useState(
+    "https://www.recipetineats.com/tachyon/2015/11/Lemon-Garlic-Chicken-Potato-Bake_7-copy.jpg?resize=900%2C1260&zoom=0.72",
+  );
+  const [ingredients, setIngredients] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    if (
+      title &&
+      instructions &&
+      ingredients.length &&
+      image &&
+      categories.length
+    ) {
+      const recipe = {
+        title: title,
+        instructions: instructions,
+        image: image,
+        ingredients: ingredients,
+        categories: categories,
+      };
+      const submitRecipe = async () => {
+        await createRecipe(recipe);
+        navigate("/");
+      };
+
+      submitRecipe();
+    }
+  }, [title, instructions, image, ingredients, categories, navigate]);
+
+  return (
+    <RecipeForm
+      username="food_gobbler"
+      profileImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgn48DSjgodT0TD3-ffVMwqefzYMBKLA5n0qSkCzlKbg&s=10"
+      setTitle={setTitle}
+      setInstructions={setInstructions}
+      setIngredients={setIngredients}
+      setImage={setImage}
+      setCategories={setCategories}
+      title={title}
+      instructions={instructions}
+      ingredients={ingredients}
+      image={image}
+      categories={categories}
+    />
+  );
+};
+export default CreateRecipe;
