c017cd2decf8a4315958fe2cc18ae58bb31bf406
nathnet <nathachanok.n@gmail.com>
2026-08-04 22:10:07 -0700

    Clear errors upon new fetches

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -40,6 +40,7 @@ export default function HomePage() {
     }
     const fetchFilteredRecipes = async () => {
       try {
+        setError(null);
         setLoading(true);
         const recipes = await getRecipes(selectedCategory);
         setRecipes(recipes);
